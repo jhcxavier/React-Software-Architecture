e936d3ff732a3e87498de6ba55be7034d3dbfe3c
João Henrique Xavier <jhcxavier@gmail.com>
2021-09-16 14:24:57 -0700

    Add server side rendering

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 import express from "express";
 import React from "react";
 import { renderToString } from "react-dom/server";
-
+import { StaticRouter } from "react-router-dom";
+import path from "path";
+import fs from "fs";
+import App from "./src/App";
 const app = express();
 
+app.use(express.static("./build", { index: false }));
+
 app.get("/*", (req, res) => {
-  const reactApp = renderToString(<h1>Hello from the Server Side</h1>);
-  return res.send(`
-        <html>
-            <body>
-                <div id="root">${reactApp}</div>
-            </body>
-        </html>
-    `);
+  const reactApp = renderToString(
+    <StaticRouter location={req.url}>
+      <App />
+    </StaticRouter>
+  );
+
+  const templateFile = path.resolve("./build/index.html");
+  fs.readFile(templateFile, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${reactApp}></div>`)
+    );
+  });
 });
 app.listen(8080, () => {
   console.log("Server is listening on port 8080");
